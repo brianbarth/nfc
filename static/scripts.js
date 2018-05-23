7505c58d4934cc26f437013150a12e06e0315982
Brian Barth <bsbarth101@yahoo.com>
2018-05-22 20:40:50 -0500

    fixing load up utterance

--- static/scripts.js
+++ static/scripts.js
@@ -5,6 +5,26 @@ function random(array) {
     document.getElementById("button2").onclick = function() { speak(word) }; 
 };
 
+function preSpeak(text, callback) {
+    var u = new SpeechSynthesisUtterance();
+    u.text = text;
+    u.lang = 'en-US';
+        
+    u.onend = function () {
+        if (callback) {
+            callback();
+        }
+    };
+
+    u.onerror = function (e) {
+        if (callback) {
+            callback(e);
+        }
+    };
+
+    speechSynthesis.speak(u);
+}
+
 // say a message
 function speak(text, callback) {
     var u = new SpeechSynthesisUtterance();
